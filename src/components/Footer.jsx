7cc12cbd6c25a1bfac480b34ed3bb2eb8f1e1fc1
motty mandel <mandelmottyisrael65@gmail.com>
2023-11-20 18:26:56 -0500

    added footer links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,9 +8,9 @@ export default function Footer() {
             <div className="row">
                 <div className="col-12 custom-footer">
                     <h4 className='footer-title'>
-                        GitHub
-                        LinkedIn
-                        Instagram
+                        <a href="https://github.com/motty-mandel" target="_blank" rel="noopener noreferrer">GitHub</a>
+                        <a href="www.linkedin.com/in/motty-mandel-7216651a1" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+                        <a href="https://www.instagram.com/mandelmotty65/" target="_blank" rel="noopener noreferrer">Instagram</a>
                     </h4>
                 </div>
             </div>
